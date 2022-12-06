dc589d968709dccc2eefc5ef7f93f4b9d75972fb
Hardik Desai <hardik.desai@education.gov.uk>
2022-12-06 11:33:20 +0000

    code cleanup

--- FamilyExperience.Dataimport/Converter/ListorSingleConverter.cs
+++ FamilyExperience.Dataimport/Converter/ListorSingleConverter.cs
@@ -13,17 +13,10 @@ namespace FamilyExperience.Dataimport.Converter
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             var token = JToken.Load(reader);
-            if (token.Type == JTokenType.Array)
-            {
-                return token.ToObject<List<T>>();
-            }
-            return new List<T> { token.ToObject<T>() };
+            return token.Type == JTokenType.Array ? token.ToObject<List<T>>() : new List<T> { token.ToObject<T>() };
         }
 
-        public override bool CanWrite
-        {
-            get { return false; }
-        }
+        public override bool CanWrite => false;
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
